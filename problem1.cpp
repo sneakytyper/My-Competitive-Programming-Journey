6697c79ded06805219a59dc69460d94c5c01589d
Sourav Sharma <souravs24@iiserb.ac.in>
2025-02-17 16:29:11 +0530

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void init_code(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif 
+}
+
+int main(){
+	init_code();
+
+	int A,B;
+	cin >> A;
+	cin >> B;
+	int x = A%B==0;
+	int y = B%A==0;
+	
+	if (A>=0 && B>=0 && A<=1e6 && B<=1e6){
+		if ( x==0 || y==0 ){
+			cout << "Multiples" << endl;
+		}
+		else{
+			cout << "No Multiples" << endl;
+		}	
+	} 
+	return 0;
+}
