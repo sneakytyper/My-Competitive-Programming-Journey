1f327289bc7959dacd00dd7f62f240f72d9e176a
Sourav Sharma <souravs24@iiserb.ac.in>
2025-02-17 16:39:09 +0530

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void init_code(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif 
+}
+
+int main(){
+	init_code();
+
+	ios_base::sync_with_stdio(0); // for more efficieny while taking cin and cout
+	cin.tie(0);
+
+	string F1, S1;
+	string F2, S2;
+
+	cin >> F1 >> S1;
+	cin >> F2 >> S2;
+
+	if (S1 == S2){
+		cout << "ARE Brothers" << "\n";
+	}
+	else{
+		cout << "NOT" << "\n";
+	}
+	return 0;
+}
