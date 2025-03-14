a9f5614431ceb2064edf662ce1a4ad2e2738b4cd
Sourav Sharma <souravs24@iiserb.ac.in>
2025-03-14 21:25:03 +0530

    Update p1.cpp

--- TLE LEVEL 1/week2/d2/p1.cpp
+++ TLE LEVEL 1/week2/d2/p1.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int N;
+    cin >> N;
+    
+    int x;
+    x = N%5;
+    
+    if(x>=3){
+    cout << N-x+5 << '\n';
+    }
+    else{
+    cout << N-x << '\n';
+    }
+}
